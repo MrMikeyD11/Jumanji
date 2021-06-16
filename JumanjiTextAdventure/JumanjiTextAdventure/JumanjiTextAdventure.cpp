d3165de7981c4a40dfc00a0eaed7978233be9b43
Michael Durst <Michael.Durst@yahoo.com>
2021-06-15 20:23:33 -0700

    Finalizing the assignment
    Added the final comments and code I wanted to the project. Added things like comments, int, and string variables. Additionally, added extra story into the adventure.

--- JumanjiTextAdventure/JumanjiTextAdventure/JumanjiTextAdventure.cpp
+++ JumanjiTextAdventure/JumanjiTextAdventure/JumanjiTextAdventure.cpp
@@ -1,6 +1,3 @@
-// Lost Fortune
-// A personalized adventure
-
 #include <iostream>
 #include <string>
 
@@ -11,7 +8,8 @@ using std::string;
 
 int main()
 {
-	// Made all of the variables seperate. Additionally, added some of my own variables. 
+	// Made all of the variables seperate. Additionally, added some of my own variables.
+	// *Added some extra int {Goblins} - and added a extra string {Dragon}
 	const int GOLD_PIECES = 9000;
 	int Dragonslayers;
 	int killed;
@@ -20,9 +18,16 @@ int main()
 	string leader;
 	string Dragon;
 
-	//Getting the variable information and initializing them. 
+	//Getting the variable information and initializing them.
+	//Rearranged where you enter information to make it flow a little better. Additionally, initialized the added variables into the text adventure. 
 	cout << "Welcome to the legend of the Dragonslayer's\n\n";
 	cout << "Please enter the required information for the Dragonslayer Adventure! \n";
+
+	cout << "Enter your name: ";
+	cin >> leader;
+
+	cout << "Enter the name of the main Dragon: ";
+	cin >> Dragon;
 
 	cout << "Enter a number: ";
 	cin >> Dragonslayers;
@@ -35,16 +40,13 @@ int main()
 
 	survivors = Dragonslayers - killed;
 
-	cout << "Enter your name: ";
-	cin >> leader;
-
-	cout << "";
-	cin >> Dragon;
+	
 
 	//Adding the dialouge for the adventure
 	cout << "\nA brave group of " << Dragonslayers << " set out on a quest \n";
 	cout << "-- in search of the lost treasure of the Ancient Dragons. \n";
 	cout << "The group was led by a infamous Knight, " << leader << ".\n";
+	// Added another encounter to the adventure, where the group is ambushed by a group of goblins
 	cout << "\nAlong the way, a band of " << Goblins << " Goblins ambushed the party. \n";
 	cout << "All fought bravely under the command of " << leader;
 	cout << ", and the Goblins were defeated with the party unscathed. \n";
@@ -55,15 +57,23 @@ int main()
 	cout << "Venturing further into the golden chambers, the Dragonslayers strucken with both amazement and fear of what lies ahead of them. \n";
 	cout << "As they deeper and deeper into the depths of the dwarven fortress they are stopped by the thunderous sound of stomping around them. \n";
 	cout << "A group of three Ancient Dragons surronding the group of Dragonslayers \n";
-	cout << "Of the Dragonslayers, " << killed << " were vanquished, ";
+	// Added some dialouge between the leader and the main dragon 
+	cout << "My name is "<< Dragon << " Why do you intrude into my domain! \n";
+	cout << "We have come to slay you for terrorizing our townspeople! \n";
+	cout << Dragon << " responded with mearly a large roar in return. \n";
+	cout << "Fighting on all side against three Ancient Dragons is not impossible task but it is certainly not a easy one. \n";
+	cout << "After a long and treacherous fight. \n";
+	cout << killed << " Dragonslayers were killed in battle. \n";
 	cout << "leaving just " << survivors << " in the group.\n";
 
 	// The ending of the text adventure 
 	cout << "\nThe party was about to give up all hope. \n";
 	cout << "But while laying the deceased to rest, ";
-	cout << "they stumbled upon the buried fortune. \n";
+	cout << "the group decided to take all of the gold they could carry back to the inn. \n";
+	cout << "Trying to figure out the what to do with the gold they carried back." << leader << "Decided to split the gold evenly among the survivors. \n"    ;
 	cout << "So the adventurers split " << GOLD_PIECES << " gold pieces.\n";
-	cout << leader << " held on to the extra " << (GOLD_PIECES % survivors);
+	// Changed how the gold was split in the end rather than have it decided through %, I went with dividing the gold piecies by survivors.
+	cout << leader << " held on to the extra " << (GOLD_PIECES / survivors);
 	cout << " pieces to keep things fair of course.\n";
 
 	return 0;
