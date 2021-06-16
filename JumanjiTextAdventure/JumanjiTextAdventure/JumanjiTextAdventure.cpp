d6ec1fb233fbba15495eebc0d2ad798f31761eca
Michael Durst <Michael.Durst@yahoo.com>
2021-06-15 19:41:46 -0700

    Updated information
    Added new variables and information to the story

--- JumanjiTextAdventure/JumanjiTextAdventure/JumanjiTextAdventure.cpp
+++ JumanjiTextAdventure/JumanjiTextAdventure/JumanjiTextAdventure.cpp
@@ -11,13 +11,18 @@ using std::string;
 
 int main()
 {
-	const int GOLD_PIECES = 90000;
-	int Dragonslayers, killed, survivors;
+	// Made all of the variables seperate. Additionally, added some of my own variables. 
+	const int GOLD_PIECES = 9000;
+	int Dragonslayers;
+	int killed;
+	int survivors;
+	int Goblins; 
 	string leader;
+	string Dragon;
 
-	//get the information
+	//Getting the variable information and initializing them. 
 	cout << "Welcome to the legend of the Dragonslayer's\n\n";
-	cout << "Please enter the following for your personalized adventure\n";
+	cout << "Please enter the required information for the Dragonslayer Adventure! \n";
 
 	cout << "Enter a number: ";
 	cin >> Dragonslayers;
@@ -25,19 +30,24 @@ int main()
 	cout << "Enter a number, smaller than the first: ";
 	cin >> killed;
 
+	cout << "Enter a number, Between 2-7: ";
+	cin >> Goblins;
+
 	survivors = Dragonslayers - killed;
 
 	cout << "Enter your name: ";
 	cin >> leader;
 
-	//tell the story
+	cout << "";
+	cin >> Dragon;
+
+	//Adding the dialouge for the adventure
 	cout << "\nA brave group of " << Dragonslayers << " set out on a quest \n";
 	cout << "-- in search of the lost treasure of the Ancient Dragons. \n";
 	cout << "The group was led by a infamous Knight, " << leader << ".\n";
-
-	cout << "\nAlong the way, a band of marauding Goblins ambushed the party. \n";
+	cout << "\nAlong the way, a band of " << Goblins << " Goblins ambushed the party. \n";
 	cout << "All fought bravely under the command of " << leader;
-	cout << ", and the Goblins were defeated. \n";
+	cout << ", and the Goblins were defeated with the party unscathed. \n";
 	cout << "After a long and treacherous journey through the Black Mountains where the group of Ancient Dragons live. \n";
 	cout << "The group arrived upon a set of towering stone doors of a former dwarven fortress.\n";
 	cout << "The group pushing open large stone doors. Immediately shown to the party is endless piles of gold. \n";
@@ -48,6 +58,7 @@ int main()
 	cout << "Of the Dragonslayers, " << killed << " were vanquished, ";
 	cout << "leaving just " << survivors << " in the group.\n";
 
+	// The ending of the text adventure 
 	cout << "\nThe party was about to give up all hope. \n";
 	cout << "But while laying the deceased to rest, ";
 	cout << "they stumbled upon the buried fortune. \n";
@@ -57,3 +68,6 @@ int main()
 
 	return 0;
 }
+
+// I acutally had some fun doing this 
+// Use this as a template for the future. 
